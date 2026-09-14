aae6a07fd17110076d8ae2e035819c5e1d1c9df3
LeVanDang92 <danglevan.9919@gmail.com>
2026-09-14 23:17:36 +0700

    update views
    update view

---
+++ WarehouseManager/WarehouseManager/WarehouseManager.Presentation.Wpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManager.Presentation.Wpf.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+    }
+}
