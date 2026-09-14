89797bb4ecb6455ad08fae15164cfb911b047835
danglv <danglevan.9919@gmail.com>
2026-09-14 22:08:18 +0700

    init warehouse

---
+++ WarehouseManager/WarehouseManager/WarehouseManager.Presentation.Wpf/ViewModels/InventoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManager.Presentation.Wpf.ViewModels
+{
+    public sealed class InventoryViewModel : ViewModelBase
+    {
+        public string Title => "Inventory Management";
+    }
+}
---
+++ WarehouseManager/WarehouseManager/WarehouseManager.Presentation.Wpf/ViewModels/MaterialsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManager.Presentation.Wpf.ViewModels
+{
+    public sealed class MaterialsViewModel : ViewModelBase
+    {
+        public string Title => "Materials Management";
+    }
+}
