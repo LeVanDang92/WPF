2974b7a1a91b13515c9b56e1defa6ed44d817ad0
danglv <danglevan.9919@gmail.com>
2026-09-17 15:16:09 +0700

    Create base code

--- WarehouseManager/WarehouseManager/WarehouseManager.Presentation.Wpf/ViewModels/MainWindowViewModel.cs
+++ WarehouseManager/WarehouseManager/WarehouseManager.Presentation.Wpf/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
-namespace WarehouseManager.Presentation.Wpf.ViewModels
+namespace WarehouseManager.Presentation.Wpf.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
 {
-    public partial class MainWindowViewModel : ViewModelBase
+    private readonly DashboardViewModel _dashboardViewModel;
+    private readonly InventoryViewModel _inventoryViewModel;
+    private readonly MaterialsViewModel _materialsViewModel;
+
+    [ObservableProperty]
+    private ViewModelBase _currentViewModel;
+
+    public MainWindowViewModel(DashboardViewModel dashboardViewModel, InventoryViewModel inventoryViewModel, MaterialsViewModel materialsViewModel)
     {
+        _dashboardViewModel = dashboardViewModel;
+        _inventoryViewModel = inventoryViewModel;
+        _materialsViewModel = materialsViewModel;
+        _currentViewModel = _dashboardViewModel;
+    }
+
+    [RelayCommand]
+    private void ShowDashboard()
+    {
+        CurrentViewModel = _dashboardViewModel;
+    }
+    
+    [RelayCommand]
+    private void ShowInventory()
+    {
+        CurrentViewModel = _inventoryViewModel;
+    }
+
+    [RelayCommand]
+    private void ShowMaterials()
+    {
+        CurrentViewModel = _materialsViewModel;
     }
 }
